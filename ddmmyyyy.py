a88c304e72a92c8f01656cc169bcc11e114196a2
DanFonR <106414190+DanFonR@users.noreply.github.com>
2023-08-23 20:13:06 -0300

    updated ddmmyyyy.py
    made this thing less bad, but still horrible

--- ddmmyyyy.py
+++ ddmmyyyy.py
@@ -1,110 +1,57 @@
-#this program uses the gregorian calendar
-#can be used for any year between 1000 ce and 2099 ce
-d = int(input("enter day:"))
-m = int(input("enter month:"))
-y = int(input("enter year:"))
-
-if y == 0:
-	y = 2021
-
-if m not in range(1, 13):
-	print("invalid month.")
-elif (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12) and d not in range(1, 32):
-	print("invalid day")
-elif (m == 4 or m == 6 or m == 9 or m == 11) and d not in range(1, 31):
-	print("invalid day")
-elif m == 2:
-	if (y % 4 == 0 and (y % 400 == 0 or y % 100 != 0)) and d not in range(1, 30):
-		print("invalid day")
-	elif d not in range(1, 29):
-		print("invalid day")
-else:
-	t = y - ((y // 100) * 100)
-	key = (t // 4 + t % 7) % 7
-	if y in range(1900, 2000):
-		key = key
-	elif y in range(2000, 2100):
-		key -= 1
-		if key == -1:
-			key = 6
-	elif y in range(1800, 1900):
-		key += 2
-		if key == 7:
-			key = 0
-		elif key == 8:
-			key = 1
-	elif y in range(1700, 1800):
-		key -= 3
-		if key == -3:
-			key = 4
-		elif key == -2:
-			key = 5
-		elif key == -1:
-			key = 6
-	elif y in range(1600, 1700):
-		key -= 1
-		if key == -1:
-			key = 6
-	elif y in range(1500, 1600):
-		key = key
-	elif y in range(1400, 1500):
-		key += 2
-		if key == 7:
-			key = 0
-		elif key == 8:
-			key = 1
-	elif y in range(1300, 1400):
-		key -= 3
-		if key == -3:
-			key = 4
-		elif key == -2:
-			key = 5
-		elif key == -1:
-			key = 6
-	elif y in range(1200, 1300):
-		key -= 1
-		if key == -1:
-			key = 6
-	elif y in range(1100, 1200):
-		key = key
-	elif y in range(1000, 1100):
-		key += 2
-		if key == 7:
-			key = 0
-		elif key == 8:
-			key = 1
-
-	if m == 1 or m == 10:
-		n = 1
-	elif m == 2 or m == 3 or m == 11:
-		n = 4
-	elif m == 4 or m == 7:
-		n = 0
-	elif m == 5:
-		n = 2
-	elif m == 6:
-		n = 5
-	elif m == 8:
-		n = 3
-	elif m == 9 or m == 12:
-		n = 6
-
-	wday = (d + n + key) % 7
-	if (m == 1 or m == 2) and (y % 4 == 0 and (y % 400 == 0 or y % 100 != 0)):
-		wday -= 1
-	else:
-		wday = wday
-	if wday == 0:
-		print("saturday")
-	elif wday == 1:
-		print("sunday")
-	elif wday == 2:
-		print("monday")
-	elif wday == 3:
-		print("tuesday")
-	elif wday == 4:
-		print("wednesday")
-	elif wday == 5:
-		print("thursday")
-	elif wday == 6:
-		print("friday")
+print("Hello! This program uses a given date to calculate which day of the week it was. It uses the Gregorian Calendar, and as such, it won't be accurate pre-1582.\n")
+def d():
+	while True:
+		try:
+			d = int(input("Please enter day: "))
+			if d not in range(1, 32):
+				raise Exception
+		except Exception:
+			print("Please enter a whole number between 1 and 31.")
+		else:
+			return d
+d = d()
+def m(d):
+	m31 = [1, 3, 5, 7, 8, 10, 12]
+	m30 = [4, 6, 9, 11]
+	while True:
+		try:
+			m = int(input("Please enter month (1-12): "))
+			if m not in range(1, 13):
+				raise ValueError
+			elif (m == 2 and d > 29) or (d > 31 and m in m31) or (d > 30 and m in m30):
+				raise Exception
+		except ValueError:
+			print("Please enter a whole number between 1 and 12.")
+		except Exception:
+				print("Please enter a valid month.")
+		else:
+			return m
+m = m(d)
+def y(m, d):
+	while True:
+		try:
+			y = int(input("Please enter year (2 digits for current century): "))
+			leap = (y % 4 == 0 and (y % 400 == 0 or y % 100 != 0))
+			if m == 2:
+				if not leap and d > 28:
+					raise ValueError
+		except ValueError:
+			print("The year is incompatible with day and month. Please try again")
+		except Exception:
+			print("Please enter a valid year.")
+		else:
+			return (y, leap)
+y, leap = y(m, d)
+twodigits = int(str(y)[-2:])
+yval = (twodigits // 4 + twodigits % 7) % 7
+if len(str(y)) == 2:
+	y += 2000
+valcor = [0, -1, -3, 2]
+yval += valcor[(((y - 1900) % 400) // 100)]
+mk = [1, 4, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6]
+mval = mk[m - 1]
+wval = (d + mval + yval) % 7
+if (m == 1 or m == 2) and leap:
+	wval -= 1
+wday = ["saturday", "sunday", "monday", "tuesday", "wednesday", "thursday", "friday"]
+print(f"That day was a {wday[wval]}.")
