3b729b5648958996b0d886525c8672d84256a6bf
DanFonR <106414190+DanFonR@users.noreply.github.com>
2023-11-20 23:00:34 +0000

    simple calculator to learn argc and argv

--- c_cpp/c/mainargstest.c
+++ c_cpp/c/mainargstest.c
@@ -1,26 +1,77 @@
+// argc = 1: std; argc = 2: which nums?; argc = 3: which num?; argc = 4: result; else: error
 #include <stdio.h>
-int main(int argc, char *argv[]){
-  int operr;
-  char oper[4][5] = {"add", "sub", "mult", "div"};
-  int ok = scanf("%d", &operr);
-  
-  switch (operr) {
-    case 0:
-      int sum = (int)argv[2]+(int)argv[3];
-      printf("%d", sum);
+#include <stdlib.h>
+
+void opt_test(int *option) {
+  while (1){
+    int test = scanf("%d", option);
+
+    getchar();
+
+    if (test == 1){
       break;
-    case 1:
-      int sub = (int)argv[2]-(int)argv[3];
-      printf("%d", sub);
-      break;
-    case 2:
-      int mult = (int)argv[2] * (int)argv[3];
-      printf("%d", mult);
-      break;
-    case 3:
-      float div = (float)argv[2] / (float)argv[3];
-      printf("%.3f", div);
-      break;
+    }
+
+    printf("invalid option\n");
   }
+}
+
+int main(int argc, char *argv[]){
+  switch (argc) {
+    case 1:
+      printf("choose between:\n\
+1. add 2 numbers\n\
+2. subtract one number from another\n\
+3. multiply 2 numbers\n\
+4. divide one number by another\n\
+0. quit\n");
+      
+      int option, num1, num2;;
+      
+      opt_test(&option);
+
+      if (option != 0) {
+        printf("insert first num: ");
+        opt_test(&num1);
+        printf("insert second num: ");
+        opt_test(&num2);
+      }
+      
+      switch (option) {
+        case 1:
+          int sum = num1 + num2;
+          
+          printf("the sum of %d and %d is %d\n", num1, num2, sum);
+
+          break;
+
+        case 2:
+          int minus = num1 - num2;
+
+          printf("%d minus %d is %d\n", num1, num2, minus);
+
+          break;
+
+        case 3:
+          int mult = num1 * num2;
+
+          printf("%d times %d is %d\n", num1, num2, mult);
+
+          break;
+
+        case 4:
+          float div = (float)num1 / num2;
+
+          printf("%d divided %d is approximately %.2f\n", num1, num2, div);
+
+          break;
+      }      
+      
+      break;
+
+    case 2:
+      
+  }
+  
   return 0;
 }
